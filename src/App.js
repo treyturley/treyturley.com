bc7187ab8712717f7a0a11425838b124e8aeabc8
treyturley <81778542+treyturley@users.noreply.github.com>
2023-01-24 17:45:19 -0600

    adds routes to the project pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Skullking from './pages/projects/Skullking';
+import FSEDataFeed from './pages/projects/FSEDataFeed';
 import Yahtzee from './pages/projects/Yahtzee';
 
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,7 +20,12 @@ function App() {
         <Container className='content'>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/projects/yahtzee' element={<Yahtzee />} />
+            <Route
+              path='/projects/skull-king-scorecard'
+              element={<Skullking />}
+            />
+            <Route path='/projects/fse-data-feed' element={<FSEDataFeed />} />
+            <Route path='/projects/yahtzee-scorecard' element={<Yahtzee />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
         </Container>
