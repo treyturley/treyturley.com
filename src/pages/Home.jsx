6f1289d056d5a4404dce2b3f5b8d84da73dcf4e2
treyturley <81778542+treyturley@users.noreply.github.com>
2023-01-24 12:38:20 -0600

    updates copy and styling for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import { Col, Row, Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -20,113 +22,204 @@ function Home() {
 
       <section id='projects'>
         <Row xs={1} md={2} className='g-5'>
-          <Col className='d-flex align-items-stretch'>
+          <Col className='d-flex align-items-stretch project-col'>
             <Card>
-              <Card.Body>
-                <Card.Title>Skull King</Card.Title>
+              <Card.Body className='d-flex flex-column justify-content-between'>
+                <div>
+                  <Card.Title>
+                    <Link to='/projects/skull-king-scorecard'>
+                      Skull King Scorecard
+                    </Link>
+                  </Card.Title>
 
-                <Card.Text>
-                  Scorekeeping in Skull King can be tricky, but this scoring app
-                  makes it a breeze. One player acts as the scorekeeper and
-                  inputs each player's bid, tricks, and bonus points for each
-                  round. The remaining players can watch scores update in
-                  realtime and see everyones bid for the current round.
-                </Card.Text>
-
-                <div className='mb-4'>
-                  <Row className='text-center'>
-                    <Col sm={{ span: 4, offset: 0 }}>
-                      <i className='fa-brands fa-react fa-2xl'></i>
-                    </Col>
-                    <Col sm={{ span: 4, offset: 0 }}>
-                      <i className='fa-brands fa-node-js fa-2xl'></i>
-                    </Col>
-                    <Col sm={{ span: 4, offset: 0 }}>
-                      <i className='fa-brands fa-bootstrap fa-2xl'></i>
-                    </Col>
-                  </Row>
-                  <Row className='text-center'>
-                    <Col sm={{ span: 4, offset: 0 }}>React</Col>
-                    <Col sm={{ span: 4, offset: 0 }}>Node.js</Col>
-                    <Col sm={{ span: 4, offset: 0 }}>Bootstrap</Col>
-                  </Row>
+                  <Card.Text>
+                    Scorekeeping in Skull King can be tricky, but this scoring
+                    app makes it a breeze. One player acts as the scorekeeper
+                    and inputs each player's bid, tricks, and bonus points for
+                    each round. The remaining players can watch scores update in
+                    realtime and see everyones bid for the current round.
+                  </Card.Text>
                 </div>
-
-                <div className='d-grid gap-2'>
-                  <Row className='text-center'>
-                    <Col sm={{ span: 3, offset: 3 }}>
-                      <Card.Link
-                        as={Button}
-                        variant='dark'
-                        href='https://treyturley.com/skullking-scorecard'
-                      >
-                        Live
-                      </Card.Link>
-                    </Col>
-                    <Col sm={{ span: 3 }}>
-                      <Card.Link
-                        as={Button}
-                        variant='dark'
-                        href='https://github.com/treyturley/sk-scorecard-react'
-                      >
-                        Code
-                      </Card.Link>
-                    </Col>
-                  </Row>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-
-          <Col className='d-flex align-items-stretch'>
-            <Card>
-              <Card.Body>
-                <Card.Title>Yahtzee Scorecard</Card.Title>
-
-                <Card.Text>
-                  A digital scorecard for Yahtzee. Calculates score totals for
-                  the upper and lower sections as well as the overall score.
-                  This is a vanilla HTML/CSS/Javascript project.
-                </Card.Text>
 
                 <div className='d-flex flex-column'>
-                  <div className='mb-4'>
+                  <div className='m-4'>
                     <Row className='text-center'>
-                      <Col sm={{ span: 4, offset: 0 }}>
-                        <i className='fa-brands fa-html5 fa-2xl icon'></i>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-react fa-2xl'></i>
                       </Col>
-                      <Col sm={{ span: 4, offset: 0 }}>
-                        <i className='fa-brands fa-css3-alt fa-2xl icon'></i>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-node-js fa-2xl'></i>
                       </Col>
-                      <Col sm={{ span: 4, offset: 0 }}>
-                        <i className='fa-brands fa-square-js fa-2xl icon'></i>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-bootstrap fa-2xl'></i>
                       </Col>
                     </Row>
                     <Row className='text-center'>
-                      <Col sm={{ span: 4, offset: 0 }}>HTML</Col>
-                      <Col sm={{ span: 4, offset: 0 }}>CSS</Col>
-                      <Col sm={{ span: 4, offset: 0 }}>JS</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>React</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>Node.js</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>Bootstrap</Col>
                     </Row>
                   </div>
 
                   <div className='d-grid gap-2'>
                     <Row className='text-center'>
-                      <Col sm={{ span: 3, offset: 3 }}>
+                      <Col>
+                        <Card.Link
+                          as={Button}
+                          variant='dark'
+                          href='https://treyturley.com/skullking-scorecard'
+                          className='my-btn-wide'
+                        >
+                          Checkout The Website
+                        </Card.Link>
+                      </Col>
+                      <Col>
+                        <Card.Link
+                          as={Button}
+                          variant='dark'
+                          href='https://github.com/treyturley/sk-scorecard-react'
+                          className='my-btn-wide'
+                        >
+                          View The Code
+                        </Card.Link>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col className='d-flex align-items-stretch project-col'>
+            <Card>
+              <Card.Body className='d-flex flex-column justify-content-between'>
+                <div>
+                  <Card.Title>
+                    <Link to='/projects/fse-data-feed'>FSE Data Feed</Link>
+                  </Card.Title>
+
+                  <Card.Text>
+                    The FSE Data Feed started as a console app that used an API
+                    provided by FSEconomy to aggregate the airline job data.
+                    This made it easy to find flights between interesting
+                    locations or the ones with the best payout. It then evolved
+                    into a REST API built with ASP.NET and a React frontend was
+                    added to surface the data.
+                  </Card.Text>
+                </div>
+
+                <div className='d-flex flex-column'>
+                  <div className='m-4'>
+                    <Row className='text-center'>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <img src='./images/dotnet.png' alt='dotnet' />
+                      </Col>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <img src='./images/api.png' alt='api' />
+                      </Col>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-react fa-2xl'></i>
+                      </Col>
+                    </Row>
+                    <Row className='text-center'>
+                      <Col xs={{ span: 4, offset: 0 }}>ASP.NET</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>REST API</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>React</Col>
+                    </Row>
+                  </div>
+
+                  <div className='d-grid gap-2'>
+                    <Row className='text-center'>
+                      <Col>
                         <Card.Link
                           as={Button}
                           variant='dark'
                           href='https://treyturley.com/yahtzee-scorecard'
+                          className='my-btn-wide'
                         >
-                          Live
+                          Checkout The Website
                         </Card.Link>
                       </Col>
-                      <Col sm={{ span: 3 }}>
+                      <Col>
                         <Card.Link
                           as={Button}
                           variant='dark'
                           href='https://github.com/treyturley/yahtzee-scorecard'
+                          className='my-btn-wide'
                         >
-                          Code
+                          View The Code
+                        </Card.Link>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row className='mt-5'>
+          <Col
+            md={{ span: 6, offset: 3 }}
+            className='d-flex align-items-stretch project-col'
+          >
+            <Card>
+              <Card.Body className='d-flex flex-column justify-content-between'>
+                <div>
+                  <Card.Title>
+                    <Link to='/projects/yahtzee-scorecard'>
+                      Yahtzee Scorecard
+                    </Link>
+                  </Card.Title>
+
+                  <Card.Text>
+                    A digital scorecard for Yahtzee. It calculates score totals
+                    for the upper and lower sections as well as the overall
+                    score. This is a super straight forward app made with HTML,
+                    CSS, and Javascript.
+                  </Card.Text>
+                </div>
+
+                <div className='d-flex flex-column'>
+                  <div className='m-4'>
+                    <Row className='text-center'>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-html5 fa-2xl icon'></i>
+                      </Col>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-css3-alt fa-2xl icon'></i>
+                      </Col>
+                      <Col xs={{ span: 4, offset: 0 }}>
+                        <i className='fa-brands fa-square-js fa-2xl icon'></i>
+                      </Col>
+                    </Row>
+                    <Row className='text-center'>
+                      <Col xs={{ span: 4, offset: 0 }}>HTML</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>CSS</Col>
+                      <Col xs={{ span: 4, offset: 0 }}>JS</Col>
+                    </Row>
+                  </div>
+
+                  <div className='d-grid gap-2'>
+                    <Row className='text-center'>
+                      <Col>
+                        <Card.Link
+                          as={Button}
+                          variant='dark'
+                          href='https://treyturley.com/yahtzee-scorecard'
+                          className='my-btn-wide'
+                        >
+                          Checkout The Website
+                        </Card.Link>
+                      </Col>
+                      <Col>
+                        <Card.Link
+                          as={Button}
+                          variant='dark'
+                          href='https://github.com/treyturley/yahtzee-scorecard'
+                          className='my-btn-wide'
+                        >
+                          View The Code
                         </Card.Link>
                       </Col>
                     </Row>
@@ -138,20 +231,27 @@ function Home() {
         </Row>
       </section>
 
-      <Row>
+      {/* <Row>
         <Col md={{ span: 6, offset: 3 }}>
-          <section id='resume'>
+          <section id='Resume' className='text-center'>
             <h1>Resume</h1>
             <a href='https://my.indeed.com/p/treyt-q42vckn'>
-              Checkout my resume!
+              Checkout my on Indeed.com
             </a>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Temporibus adipisci officiis error, suscipit quam asperiores.
-            </p>
           </section>
         </Col>
       </Row>
+
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <section id='Contact' className='text-center'>
+            <h1>Contact</h1>
+            <a href='https://my.indeed.com/p/treyt-q42vckn'>
+              Checkout my on Indeed.com
+            </a>
+          </section>
+        </Col>
+      </Row> */}
     </>
   );
 }
