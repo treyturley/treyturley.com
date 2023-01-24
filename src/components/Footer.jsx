78fbf1c93abde3ff566d25fa4adeef8898a91962
treyturley <81778542+treyturley@users.noreply.github.com>
2023-01-24 12:37:53 -0600

    removes placeholder links and adds correct links and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -7,36 +8,53 @@ function Footer() {
       <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
         <p className='col-md-4 mb-0 text-muted'>&copy; 2023 Trey Turley</p>
 
-        <ul className='nav col-md-4 justify-content-end flex-nowrap'>
-          <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+        <ul className='nav col-md-5'>
+          <li className='nav-item my-nav'>
+            <a href='/' className='nav-link px-2 text-muted '>
               Home
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a
+              href='https://my.indeed.com/p/treyt-q42vckn'
+              target='_blank'
+              rel='noreferrer'
+              className='nav-link px-2 text-muted'
+            >
               Resume
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a
+              href='mailto:tturley08+contact@gmail.com'
+              target='_blank'
+              rel='noreferrer'
+              className='nav-link px-2 text-muted'
+            >
               Contact
             </a>
           </li>
+          {/* <li className='nav-item'>
+            <Link
+              to='/projects'
+              className='nav-link px-2 text-muted text-nowrap'
+            >
+              Projects
+            </Link>
+          </li> */}
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted text-nowrap'>
-              Project Details
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted text-nowrap'>
+            <Link
+              to='/skullking-scorecard'
+              className='nav-link px-2 text-muted text-nowrap'
+            >
               Skull King Scorecard
-            </a>
+            </Link>
           </li>
         </ul>
         <a
           href='https://www.flaticon.com/free-icons/portal'
           title='portal icons'
+          className='attribution-link text-muted'
         >
           Portal icons created by Pixel perfect - Flaticon
         </a>
