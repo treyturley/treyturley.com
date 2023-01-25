c671ae120f372233e539df0739b8db7d8c7797af
treyturley <81778542+treyturley@users.noreply.github.com>
2023-01-25 13:57:30 -0600

    adds pagination to swiper and improves css for mobile devices

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,33 +1,38 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Navigation, Pagination } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 function Slider({ images }) {
   if (!images) {
     images = [];
   }
 
-  // TODO: fix css for mobile devices
-
   return (
-    <Swiper navigation={true} modules={[Navigation]}>
+    <Swiper
+      navigation={true}
+      pagination={{
+        type: 'bullets',
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+    >
       {images.map((image) => (
         <SwiperSlide key={image.description}>
           <div className='swiper-slide-container'>
             <p className='swiper-slide-text'>{image.description}</p>
-            <div
-              style={{
-                background: `url(${image.location}) center no-repeat`,
-                // backgroundPositionY: '42px',
-                backgroundSize: 'contain',
-              }}
-              className='swiper-slide-div'
-            >
-              <a href={image.location}></a>
+            <div className='swiper-slide-image-container'>
+              <div
+                style={{
+                  background: `url(${image.imagePath}) center no-repeat`,
+                  backgroundSize: 'contain',
+                }}
+                className='swiper-slide-img-div'
+              ></div>
             </div>
           </div>
         </SwiperSlide>
